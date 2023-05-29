a8a33939b114580d1fa82f16c3ba29ef23378eee
sherylann-hc <sheryl@hashicorp.com>
2023-05-29 13:20:25 +0800

    Changed size of EC2 instance

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t2.large"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "I've changed the website address" > /var/www/html/index.html
+              echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
